2ddbc822e770108900d071d8fbdd39e0e5a1a40d
Antti Räisälä <antti.raisala@gmail.com>
2016-06-13 10:26:44 +0300

    Added a JUnit-test-class

---
+++ src/test/java/javalanguagetests/CompoundAssignmentOperatorTests.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javalanguagetests;
+
+import org.junit.Test;
+
+
+/**
+ *
+ * @author anttir
+ */
+public class CompoundAssignmentOperatorTests {
+    @Test
+    public void compoundAssignmentOperatorTests(){
+        CompoundAssignmentOperator cao = new CompoundAssignmentOperator();
+    }
+            
+}
