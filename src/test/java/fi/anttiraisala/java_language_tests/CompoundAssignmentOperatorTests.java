a94fd884d30e240a08d8a77382e8a6bb2e1828ee
Antti Räisälä <antti.raisala@gmail.com>
2016-06-14 23:26:37 +0300

    Refactored a package's name

--- src/test/java/fi/anttiraisala/java_language_tests/CompoundAssignmentOperatorTests.java
+++ src/test/java/fi/anttiraisala/java_language_tests/CompoundAssignmentOperatorTests.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package fi.anttiraisala.javalanguagetests;
+package fi.anttiraisala.java_language_tests;
 
 import org.junit.Test;
 
